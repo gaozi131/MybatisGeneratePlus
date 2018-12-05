5a4fd3cc316dc697d5c625971b7e67c77e78acc7
gaozi131 <rqxiong@126.com>
2018-12-05 11:16:39 +0800

    增加float类型的重写支持

--- src/main/java/com/yaodela/gen/MyJavaTypeResolverDefaultImpl.java
+++ src/main/java/com/yaodela/gen/MyJavaTypeResolverDefaultImpl.java
@@ -5,7 +5,7 @@ import java.sql.Types;
 import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
 import org.mybatis.generator.internal.types.JavaTypeResolverDefaultImpl;
 /**
- * 重写它的数据类型对应关系  不要它默认的包装数据类型
+ * 重写它的数据类型对应关系  不要它默认的包装数据类型 因为包装类型默认值都是null 对插入时不方便
  * @author Mr.X<br/>
  * 2018年9月15日<br/>
  * E-mail:342266230@qq.com
@@ -19,5 +19,6 @@ public class MyJavaTypeResolverDefaultImpl extends JavaTypeResolverDefaultImpl {
         super.typeMap.put(Types.SMALLINT, new JdbcTypeInformation("SMALLINT", new FullyQualifiedJavaType(int.class.getName())));
         super.typeMap.put(Types.BIGINT, new JdbcTypeInformation("BIGINT", new FullyQualifiedJavaType(long.class.getName())));
         super.typeMap.put(Types.DOUBLE, new JdbcTypeInformation("BIGDECIMAL", new FullyQualifiedJavaType(double.class.getName())));
+        super.typeMap.put(Types.FLOAT, new JdbcTypeInformation("FLOAT", new FullyQualifiedJavaType(float.class.getName())));
 	}
 }
